19eacf9c2c660030db5cfc2b72e307fe46e3abe6
NIKITA <sinrulyu@gmail.com>
2024-09-30 22:30:34 +0800

    publish

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '',
+	base: 'https://nikitaplanet.github.io/gramicci/web/',
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src'),
