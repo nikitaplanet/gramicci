9036b7f69a7d81242abf31692d5030a5ab271374
taco <wkg343@gmail.com>
2024-10-02 00:49:09 +0800

    feat:點擊儲存模板出現Modal

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -20,15 +20,19 @@
 			<ExportTemplate :savedData="savedData" />
 		</FullPageModal>
 	</transition>
+
+	<SaveTemplateModal v-model:modelValue="isShowSaveTemplate" :templateValue="savedData" title="儲存模板" />
 </template>
 <script lang="ts" setup>
-import {ref} from 'vue';
+import {ref, computed} from 'vue';
 import NavBar from '@components/organisms/nav/NavBar.vue';
 import EditorBlock from '@components/organisms/editor/EditorBlock.vue';
 import Menu from '@components/organisms/nav/components/Menu.vue';
 import Logo from '@components/organisms/nav/components/Logo.vue';
 import FullPageModal from '@components/organisms/modal/FullPageModal.vue';
 import ExportTemplate from '@components/organisms/editor/ExportTemplate.vue';
+import SaveTemplateModal from '@components/organisms/save/saveTemplateModal.vue';
+import {teleportProps} from 'element-plus/lib/components/teleport/index.js';
 
 const editorRef = ref(null);
 const savedData = ref(null);
@@ -36,6 +40,7 @@ const savedData = ref(null);
 const isShowExport = ref(false);
 
 const saveTemplate = () => {
+	isShowSaveTemplate.value = true;
 	if (editorRef.value) {
 		savedData.value = editorRef.value.getEditorData();
 	}
@@ -50,6 +55,9 @@ const exportTemplate = async () => {
 		console.log('savedData.value ', savedData.value.blocks);
 	}
 };
+
+// 儲存模板
+const isShowSaveTemplate = ref(false);
 </script>
 
 <style lang="scss"></style>
