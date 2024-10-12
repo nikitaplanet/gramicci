e3d890fc84141e68974fd6a69070bfaa2a95473e
taco <wkg343@gmail.com>
2024-10-12 22:19:50 +0800

    feat:串接get模板API

---
+++ src/server/composables/useQueryTemplates.ts
@@ -0,0 +1,52 @@
+import {useQuery, useQueryClient} from '@tanstack/vue-query';
+import {ref} from 'vue';
+
+import queryTemplates from '../api/queryTemplates';
+
+interface UseQueryTemplatesOption {
+	params: {tagName: string};
+}
+
+export default (options: UseQueryTemplatesOption) => {
+	const queryClient = useQueryClient();
+
+	const params = ref(options.params);
+	const queryKey = ['QUERY_TEMPLATES', params];
+
+	const queryFn = async () => {
+		const result = await queryTemplates(params.value);
+		const data = transData(result.values);
+
+		return {
+			data,
+		};
+	};
+
+	const invalidate = () => queryClient.invalidateQueries({queryKey});
+
+	const query = useQuery({
+		enabled: () => !!params.value,
+		queryKey,
+		queryFn,
+	});
+
+	return Object.assign(query, {
+		invalidate,
+		queryKey,
+		params,
+	});
+};
+
+export function transData(data: []) {
+	const json = [];
+
+	for (let i = 1; i < data.length; i++) {
+		// 從 1 開始以跳過標題
+		const row = {};
+		for (let j = 0; j < data[0].length; j++) {
+			row[data[0][j]] = data[i][j];
+		}
+		json.push(row);
+	}
+	return json;
+}
