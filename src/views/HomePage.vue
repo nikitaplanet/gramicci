7deeb83f592e9a2c4932fb7f7a16303b71a0f3db
nikitamacbook <sinrulyu@gmail.com>
2025-08-04 00:48:05 +0800

    update copyright

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,18 @@
 <template>
 	<!--Editor-->
-	<div class="w-full">
-		<div>
+	<div class="w-full overflow-y-hidden">
+		<div class="teditor">
 			<TiptapEditor ref="editorRef" />
 		</div>
+		<div class="w-full text-xs text-center py-2 text-gray-400 italic">Copyright © 2025 GRAMICCI v1.2 / By Nikita Lyu</div>
 	</div>
 </template>
 <script lang="ts" setup>
 import TiptapEditor from '@components/organisms/editor/TiptapEditor.vue';
 </script>
+
+<style lang="scss" scoped>
+.teditor {
+	min-height: calc(100vh - 32px);
+}
+</style>
